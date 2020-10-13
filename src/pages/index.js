de757672b53b0bc9fac675f34055191941542cc0
Adam Hussain <adamh93@verizon.net>
2020-10-12 20:07:32 -0400

    done: Create cards for stats section; todo: Create header, Create component for stats list

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,17 @@
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import { getArticles } from '../services/news/client'
+import Card from '../../components/card'
 
 export default function Home () {
   const [articles, setArticles] = useState(null)
 
-  useEffect(() => {
-    if (!articles) {
-      getArticles()
-        .then(data => setArticles(data.articles))
-    }
-  })
+  // useEffect(() => {
+  //   if (!articles) {
+  //     getArticles()
+  //       .then(data => setArticles(data.articles))
+  //   }
+  // })
 
   console.log(articles)
 
@@ -26,6 +27,30 @@ export default function Home () {
         <h1>
           Welcome to <span style={{ color: '#E84393' }}>Pandem</span>Today
         </h1>
+
+        <Card>
+          <h2 className="card-title">Cases</h2>
+          <div className="stats-area">
+            <div class="green-arrow-down"></div>
+            <h1 className="case-numbers">34.2M</h1>
+          </div>
+        </Card>
+
+        <Card>
+          <h2 className="card-title">Recovered</h2>
+          <div className="stats-area">
+            <div class="green-arrow-up"></div>
+            <h1 className="case-numbers">34.2M</h1>
+          </div>
+        </Card>
+
+        <Card>
+          <h2 className="card-title">Deaths</h2>
+          <div className="stats-area">
+            <div class="red-arrow-up"></div>
+            <h1 className="case-numbers">34.2M</h1>
+          </div>
+        </Card>
       </main>
     </div>
   )
