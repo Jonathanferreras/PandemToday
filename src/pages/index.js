8986bd5f2f41f262f22ebca9b2efdad2a562e0ae
Adam Hussain <adamh93@verizon.net>
2020-10-17 13:24:05 -0400

    done: Moved Stats area into Stats component and then replace code in index.js with Stats comp tag, Adjust styling of case-numbers class to make #s line properly

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@
 import Head from 'next/head'
 import Layout from '../../components/Layout'
 import Navbar from '../../components/Navbar'
+import Stats from '../../components/Stats'
 import { useEffect, useState } from 'react'
 import { getArticles } from '../services/news/client'
-import Card from '../../components/card'
 
 
 
@@ -31,40 +31,7 @@ export default function Home () {
       <main className="content">
 
 
-        <div className="stats">
-          <h1>
-            Stats
-          </h1>
-          <div className="filter">
-            <h4 className="stats-filter">Global</h4>
-            <h5 className="see-more">See More</h5>
-          </div>
-
-
-            <Card>
-              <h2 className="card-title">Cases</h2>
-              <div className="stats-area">
-                <div className="green-arrow-down"></div>
-                <h1 className="case-numbers">34.2M</h1>
-              </div>
-            </Card>
-
-            <Card>
-              <h2 className="card-title">Recovered</h2>
-              <div className="stats-area">
-                <div className="green-arrow-up"></div>
-                <h1 className="case-numbers">23.7M</h1>
-              </div>
-            </Card>
-
-            <Card>
-              <h2 className="card-title">Deaths</h2>
-              <div className="stats-area">
-                <div className= "red-arrow-up"></div>
-                <h1 className="case-numbers">1.02M</h1>
-              </div>
-            </Card>
-        </div>
+        <Stats />
 
 
       </main>
