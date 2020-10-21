56f9d1db90a0a1d45143bdfd52e7d40b172921ec
Adam Hussain <adamh93@verizon.net>
2020-10-20 21:51:09 -0400

    done: Modify Tweets component to render tweets via map function

--- components/Tweets.js
+++ components/Tweets.js
@@ -9,23 +9,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faShareSquare } from '@fortawesome/free-solid-svg-icons';
 
 export default function Tweets(){
+
+  const tweets = [
+    {id: 1, username:'CNN', screename: 'TwitterAPI', profile_pic: "/mnews-cv19.png", test_img: 'https://pbs.twimg.com/profile_images/942858479592554497/BbazLO9L_normal.jpg', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere dolor id sapien laoreet vulputate.', date:'4:19 PM · Oct 10, 2018'},
+    {id: 2, username:'Twitter API', screename: 'TwitterAPI', profile_pic: "/mnews-cv19.png", test_img: 'https://pbs.twimg.com/profile_images/942858479592554497/BbazLO9L_normal.jpg', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere dolor id sapien laoreet vulputate.', date:'4:19 PM · Oct 10, 2018'}
+  ];
+
+  const renderTweets = tweets.map((tweet) => {
+        return(
+          <TweetCard className="tweet-area" key={tweet.id}>
+            <img className="tweet-profile-pic" src={`${tweet.profile_pic}`} />
+            <div className="tweet">
+              <div className="tweet-user">
+                <h7 className="twitter-username">{tweet.username}</h7>
+                <h7 className="twitter-handle">@{tweet.username}</h7>
+              </div>
+              <p className="tweet-body">
+                {tweet.body}
+              </p>
+            </div>
+          </TweetCard>
+        )
+      })
+
   return(
     <div className="tweets">
       <div className="heading">
         <h1 className="title">Tweets</h1>
       </div>
-      <TweetCard className="tweet-area">
-        <img className="tweet-profile-pic" src="/mnews-cv19.png" />
-        <div className="tweet">
-          <div className="tweet-user">
-            <h7 className="twitter-username">CNN</h7>
-            <h7 className="twitter-handle">@CNN</h7>
-          </div>
-          <p className="tweet-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut posuere dolor id sapien laoreet vulputate.
-          </p>
-        </div>
-      </TweetCard>
+      {renderTweets}
     </div>
   )
 }
