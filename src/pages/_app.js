90abbd085a79d8411c4c9ea9f9690a9674f33b10
Adam Hussain <adamh93@verizon.net>
2020-12-28 21:20:03 -0500

    done: Finish News page, Create cards for each news story within News page, Refactor news.js to use getArticlesByPage method to retrieve articles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 import '../styles/globals.css'
 
+
 function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- src/pages/news.js
+++ src/pages/news.js
@@ -2,18 +2,72 @@
 /* eslint-disable react/react-in-jsx-scope */
 import Link from 'next/link'
 import Head from 'next/head'
+import { useState, useEffect } from 'react'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import your icons
 import { faEye, faShareSquare } from '@fortawesome/free-solid-svg-icons'
-import Navbar from '../components/Navbar/Navbar'
+import styles from './news.module.css'
+import Layout from '../components/Layout'
+import Card from '../components/Card/Card'
 
+import { getArticlesByPage } from '../services/news/client'
 
 export default function News (props) {
-    return(
-        <div>
-            <Navbar />
-            <h3>news</h3>
-        </div>
+  const [data, setData] = useState([])
+  const [dataLoading, setDataLoading] = useState(false)
+
+  const news_articles = [
+    { title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', news_img: '/mnews-cv19.png' },
+    { title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', news_img: '/mnews-cv19.png' },
+    { title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', news_img: '/mnews-cv19.png' },
+    { title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', news_img: '/mnews-cv19.png' }
+  ]
+
+  useEffect(() => {
+    getArticlesByPage()
+      .then(d => {
+        setData(d.articles)
+        setDataLoading(true)
+        console.log(d)
+      })
+  }, [])
+
+  const loadingAnimation = () => {
+    return (
+      <div className="loading bar">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     )
+  }
+
+  const renderNews = data.map((article, article_num) => {
+    return (
+      <Card className="news-item" key={article_num}>
+        <Link href={article.url}>
+          <a>
+            <img className="news-item-img" src={article.urlToImage} />
+            <div className="news-item-story">
+              <p >{article.title}</p>
+            </div>
+          </a>
+        </Link>
+      </Card>
+    )
+  })
+
+  return (
+    <Layout>
+      <div className="news-card-area">
+        {dataLoading ? renderNews : loadingAnimation()}
+      </div>
+    </Layout>
+  )
 }
